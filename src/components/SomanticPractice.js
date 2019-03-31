0fe14e05dbe7ebfa240604c6931dd5b49d16cf19
lanners-marshall <lanners.marshall@gmail.com>
2019-03-30 17:28:53 -0700

    having fun practicing somantic ui

--- src/components/SomanticPractice.js
+++ src/components/SomanticPractice.js
@@ -51,6 +51,149 @@ class SomanticPractice extends React.Component {
 				    </Grid.Column>
 				  </Grid>
 		    </Segment>
+		    <ul>
+					<i class="angle double down icon"></i>
+					<i class="angle double left icon"></i>
+					<i class="angle double right icon"></i>
+					<i class="angle double up icon"></i>
+					<i class="angle down icon"></i>
+					<i class="angle left icon"></i>
+					<i class="angle right icon"></i>
+					<i class="angle up icon"></i>
+					<i class="arrow alternate circle down icon"></i>
+					<i class="arrow alternate circle down outline icon"></i>
+					<i class="arrow alternate circle left icon"></i>
+					<i class="arrow alternate circle left outline icon"></i>
+					<i class="arrow alternate circle right icon"></i>
+					<i class="arrow alternate circle right outline icon"></i>
+					<i class="arrow alternate circle up icon"></i>
+					<i class="arrow alternate circle up outline icon"></i>
+					<i class="arrow circle down icon"></i>
+					<i class="arrow circle left icon"></i>
+					<i class="arrow circle right icon"></i>
+					<i class="arrow circle up icon"></i>
+					<i class="arrow down icon"></i>
+					<i class="arrow left icon"></i>
+					<i class="arrow right icon"></i>
+					<i class="arrow up icon"></i>
+					<i class="arrows alternate icon"></i>
+					<i class="arrows alternate horizontal icon"></i>
+					<i class="arrows alternate vertical icon"></i>
+					<i class="caret down icon"></i>
+					<i class="caret left icon"></i>
+					<i class="caret right icon"></i>
+					<i class="caret square down icon"></i>
+					<i class="caret square down outline icon"></i>
+					<i class="caret square left icon"></i>
+					<i class="caret square left outline icon"></i>
+					<i class="caret square right icon"></i>
+					<i class="caret square right outline icon"></i>
+					<i class="caret square up icon"></i>
+					<i class="caret square up outline icon"></i>
+					<i class="caret up icon"></i>
+					<i class="cart arrow down icon"></i>
+					<i class="chart line icon"></i>
+					<i class="chevron circle down icon"></i>
+					<i class="chevron circle left icon"></i>
+					<i class="chevron circle right icon"></i>
+					<i class="chevron circle up icon"></i>
+					<i class="chevron down icon"></i>
+					<i class="chevron left icon"></i>
+					<i class="chevron right icon"></i>
+					<i class="chevron up icon"></i>
+					<i class="cloud download icon"></i>
+					<i class="cloud upload icon"></i>
+					<i class="download icon"></i>
+					<i class="exchange alternate icon"></i>
+					<i class="expand arrows alternate icon"></i>
+					<i class="external alternate icon"></i>
+					<i class="external square alternate icon"></i>
+					<i class="hand point down icon"></i>
+					<i class="hand point down outline icon"></i>
+					<i class="hand point left icon"></i>
+					<i class="hand point left outline icon"></i>
+					<i class="hand point right icon"></i>
+					<i class="hand point right outline icon"></i>
+					<i class="hand point up icon"></i>
+					<i class="hand point up outline icon"></i>
+					<i class="hand pointer icon"></i>
+					<i class="hand pointer outline icon"></i>
+					<i class="history icon"></i>
+					<i class="level down alternate icon"></i>
+					<i class="level up alternate icon"></i>
+					<i class="location arrow icon"></i>
+					<i class="long arrow alternate down icon"></i>
+					<i class="long arrow alternate left icon"></i>
+					<i class="long arrow alternate right icon"></i>
+					<i class="long arrow alternate up icon"></i>
+					<i class="mouse pointer icon"></i>
+					<i class="play icon"></i>
+					<i class="random icon"></i>
+					<i class="recycle icon"></i>
+					<i class="redo icon"></i>
+					<i class="redo alternate icon"></i>
+					<i class="reply icon"></i>
+					<i class="reply all icon"></i>
+					<i class="retweet icon"></i>
+					<i class="share icon"></i>
+					<i class="share square icon"></i>
+					<i class="share square outline icon"></i>
+					<i class="sign in alternate icon"></i>
+					<i class="sign out alternate icon"></i>
+					<i class="sort icon"></i>
+					<i class="sort alphabet down icon"></i>
+					<i class="sort alphabet up icon"></i>
+					<i class="sort amount down icon"></i>
+					<i class="sort amount up icon"></i>
+					<i class="sort down icon"></i>
+					<i class="sort numeric down icon"></i>
+					<i class="sort numeric up icon"></i>
+					<i class="sort up icon"></i>
+					<i class="sync icon"></i>
+					<i class="sync alternate icon"></i>
+					<i class="text height icon"></i>
+					<i class="text width icon"></i>
+					<i class="undo icon"></i>
+					<i class="undo alternate icon"></i>
+					<i class="upload icon"></i>
+		    </ul>
+				<div class="ui ordered steps">
+				  <div class="completed step">
+				    <div class="content">
+				      <div class="title">Shipping</div>
+				      <div class="description">Choose your shipping options</div>
+				    </div>
+				  </div>
+				  <div class="completed step">
+				    <div class="content">
+				      <div class="title">Billing</div>
+				      <div class="description">Enter billing information</div>
+				    </div>
+				  </div>
+				  <div class="active step">
+				    <div class="content">
+				      <div class="title">Confirm Order</div>
+				      <div class="description">Verify order details</div>
+				    </div>
+				  </div>
+				</div>
+				<form class="ui form">
+				  <div class="field">
+				    <label>First Name</label>
+				    <input type="text" name="first-name" placeholder="First Name"/>
+				  </div>
+				  <div class="field">
+				    <label>Last Name</label>
+				    <input type="text" name="last-name" placeholder="Last Name"/>
+				  </div>
+				  <div class="field">
+				    <div class="ui checkbox">
+				      <input type="checkbox" tabindex="0" class="hidden"/>
+				      <label>I agree to the Terms and Conditions</label>
+				    </div>
+				  </div>
+				  <button class="ui button" type="submit">Submit</button>
+				</form>
 			</Container>
 		)
 	}
